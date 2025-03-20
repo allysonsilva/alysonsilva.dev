5bc80d9963a5e6ee62bab2629d8b5dcea5d51c70
Alyson Silva <19507773+allysonsilva@users.noreply.github.com>
2025-03-19 21:33:29 -0300

    force run

--- app/Support/Helpers/General.php
+++ app/Support/Helpers/General.php
@@ -3,6 +3,12 @@
 use App\Support\Helpers\MixLocal;
 use Illuminate\Support\HtmlString;
 
+/**
+ * @param string $path
+ * @param string $manifestDirectory
+ *
+ * @return \Illuminate\Support\HtmlString|string
+ */
 function mix_local(string $path, string $manifestDirectory = ''): HtmlString|string
 {
     return app(MixLocal::class)(...func_get_args());
